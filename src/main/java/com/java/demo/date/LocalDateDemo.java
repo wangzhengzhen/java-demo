824effff766f7a13c7892b00b9d4526af7c929cf
wangzhengzhen <zz@xf1024.com>
2020-08-03 10:40:18 +0800

    Java8中取代SipmleDateFormat和Calendaer，DateTimeFormatter和LocalDate

---
+++ src/main/java/com/java/demo/date/LocalDateDemo.java
@@ -0,0 +1,34 @@
+package com.java.demo.date;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+/**
+ * DateTimeFormatter 线程安全 ，SimpleDateFormat 非线程安全
+ * DateTimeFormatter是Java8提供的新方式
+ * 单线程环境下DateTimeFormatter和LocalDateTime的parse性能比SimpleDateFormat的parse性能高
+ * 多线程环境下DateTimeFormatter作为共享变量时，大部分情况下性能比SimpleDateFormat高(因为多线程环境的复杂性，此结论不一定准确)
+ *
+ * LocalDateTime.now() 是不带有时区的，所以在被是带有时区的DateTimeFormatter格式化的时候，会报错
+ * 被带有时区的DateTimeFormatter格式化的时候需要使用ZonedDateTime.now()
+ */
+public class LocalDateDemo {
+
+    public static void main(String[] args) {
+
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+
+        // 自定义格式化日期
+        DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
+        builder.appendPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter formatter = builder.toFormatter();
+        System.out.println(formatter.format(localDateTime));
+    }
+
+}
